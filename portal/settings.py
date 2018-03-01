e4c6b02b55509afdf177d00610e33d234a009c95
mich71095 <michaelpsy71095@gmail.com>
2018-03-01 14:23:47 +0800

    Fix settings and Add 'templates' folder

--- portal/settings.py
+++ portal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r-24ozfu$7_=kz%wwse5n(o00jp+4rn)9dyvl8t)ez&j&)(2yj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
