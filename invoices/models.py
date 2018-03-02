724d2700127a6ab033ef4c2670352b9e727412fb
cholorecto <pocholorecto123@gmail.com>
2018-03-02 19:05:08 +0800

    Refactor models and model test

--- invoices/models.py
+++ invoices/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Invoice(models.Model):
     """ Model for invoices
     """
-
     DUE_RECEIPT = 'Due of Receipt'
     DUE_END_NEXT_MONTH = 'Due End of Next Month'
     DUE_END_MONTH = 'Due End of Month'
@@ -26,26 +25,25 @@ class Invoice(models.Model):
     code = models.CharField(max_length=16)
     order_id = models.CharField(max_length=16,null=True, blank=True)
     invoice_date = models.DateTimeField()
-    terms = models.CharField(max_length=16,choices=TERMS)
+    terms = models.CharField(max_length=16, choices=TERMS, default=DUE_RECEIPT)
     due_date = models.DateTimeField()
     notes = models.TextField(null=True, blank=True)
-    conditions = models.CharField(max_length=28,null=True, blank=True)
+    conditions = models.CharField(max_length=28, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=16, choices=STATUS)
+    status = models.CharField(max_length=16, choices=STATUS, default=SENT)
 
     def __str__(self):
-        return '{}'.format(self.code)
+        return f'{self.code}'
 
 
 class Item(models.Model):
     """ Model for items in invoice
     """
-
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
     details = models.CharField(max_length=255)
-    quantity = models.IntegerField()
-    rate = models.IntegerField()
+    quantity = models.IntegerField(default=0)
+    rate = models.IntegerField(default=0)
 
     def __str__(self):
-        return '{}'.format(self.invoice)
+        return f'{self.invoice}'
--- invoices/tests/models_test.py
+++ invoices/tests/models_test.py
@@ -2,87 +2,143 @@ from django.test import TestCase
 from invoices.models import Invoice, Item
 import datetime
 
-class InvoiceTestCase(TestCase):
+
+class InvoiceData(object):
+    """ Invoice data and create invoice
+    """
+    date = datetime.datetime.now()
+    invoice_data = {
+        'code':"1234ssdsd",
+        'order_id':"12",
+        'invoice_date':date,
+        'terms':"Due of Receipt",
+        'due_date':date + datetime.timedelta(days=5),
+        'notes':"sample notes",
+        'conditions':"condition 1",
+        'date_created':date,
+        'date_updated':date,
+        'status':"Sent"
+    }
+
+    def create_invoice(self):
+        data = Invoice.objects.create(
+                code = self.invoice_data['code'],
+                order_id = self.invoice_data['order_id'],
+                invoice_date = self.invoice_data['invoice_date'],
+                terms = self.invoice_data['terms'],
+                due_date = self.invoice_data['due_date'],
+                notes = self.invoice_data['notes'],
+                conditions = self.invoice_data['conditions'],
+                date_created = self.invoice_data['date_created'],
+                date_updated = self.invoice_data['date_updated'],
+                status = self.invoice_data['status']
+            )
+        return data
+
+
+class InvoiceDataFails(object):
+    """ Invoice data with errors and create invoice
+    """
+    date = datetime.datetime.now()
+    invoice_data = {
+        'code':None,
+        'order_id':"12",
+        'invoice_date':None,
+        'terms':"Due of Receipt",
+        'due_date':None,
+        'notes':"sample notes",
+        'conditions':"condition 1",
+        'date_created':date,
+        'date_updated':date,
+        'status':"Sent"
+    }
+
+    def create_invoice(self):
+        data = Invoice.objects.create(
+                code = self.invoice_data['code'],
+                order_id = self.invoice_data['order_id'],
+                invoice_date = self.invoice_data['invoice_date'],
+                terms = self.invoice_data['terms'],
+                due_date = self.invoice_data['due_date'],
+                notes = self.invoice_data['notes'],
+                conditions = self.invoice_data['conditions'],
+                date_created = self.invoice_data['date_created'],
+                date_updated = self.invoice_data['date_updated'],
+                status = self.invoice_data['status']
+            )
+        return data
+
+
+class InvoiceTestCaseSuccess(TestCase, InvoiceData):
     """ Test case for invoice model
     """
-
     def setUp(self):
-        date = datetime.datetime.now()
-        Invoice.objects.create(
-            code = "1234sdfg",
-            order_id = "12",
-            invoice_date = date,
-            terms ='Due of Receipt',
-            due_date = date + datetime.timedelta(days=5),
-            notes = "sample notes",
-            conditions = "condition 1",
-            date_created = date,
-            date_updated = date,
-            status = "Sent"
-        )
-
-        self.data = Invoice.objects.last()
+        self.create_invoice()
 
     def test_add_invoice(self):
-        self.assertEqual(self.data.code, '1234sdfg')
+        invoice = Invoice.objects.get(code=self.invoice_data['code'])
+        self.assertEqual(invoice.code, self.invoice_data['code'])
 
     def test_edit_invoice(self):
-        invoice = self.data
-        invoice.code = "new_code"
+        new_code = "new_code"
+        invoice = Invoice.objects.get(code=self.invoice_data['code'])
+        invoice.code = new_code
         invoice.order_id = "11"
         invoice.save()
-        new_invoice = Invoice.objects.last()
-        self.assertEqual(new_invoice.code, "new_code")
+        new_invoice = Invoice.objects.get(code=new_code)
+        self.assertEqual(new_invoice.code, new_code)
 
     def test_delete_invoice(self):
-        invoice = Invoice.objects.get(code="1234sdfg")
+        invoice = Invoice.objects.get(code=self.invoice_data['code'])
         invoice.delete()
 
         data = Invoice.objects.all()
         self.assertEqual(data.count(), 0)
 
 
-class InvoiceItemTestCase(TestCase):
+class InvoiceItemTestCaseSuccess(TestCase, InvoiceData):
     """ Test case for Item in voice
     """
-
     def setUp(self):
-        date = datetime.datetime.now()
-        invoice = Invoice.objects.create(
-            code = "1234sdfg",
-            order_id = "12",
-            invoice_date = date,
-            terms ='Due of Receipt',
-            due_date = date + datetime.timedelta(days=5),
-            notes = "sample notes",
-            conditions = "condition 1",
-            date_created = date,
-            date_updated = date,
-            status = "Sent"
-        )
+        invoice = self.create_invoice()
+        item_data = {
+            'invoice':invoice,
+            'details':"Detail sample",
+            'quantity':3,
+            'rate':45
+        }
         Item.objects.create(
-                invoice = invoice,
-                details = "Detail sample",
-                quantity = 12,
-                rate = 45
-            )
-
-        self.data = Item.objects.last()
+            invoice = item_data['invoice'],
+            details = item_data['details'],
+            quantity = item_data['quantity'],
+            rate = item_data['rate']
+        )
 
     def test_add_item(self):
-        self.assertEqual(self.data.invoice.code, "1234sdfg")
+        item = Item.objects.get(invoice__code=self.invoice_data['code'])
+        self.assertEqual(item.invoice.code, self.invoice_data['code'])
 
     def test_edit_item(self):
-        item = self.data
-        item.details = "New detail"
+        new_detail = "New detail"
+        item = Item.objects.get(invoice__code=self.invoice_data['code'])
+        item.details = new_detail
         item.save()
 
-        new_item = Item.objects.last()
-        self.assertEqual(new_item.details, "New detail")
+        new_item = Item.objects.get(invoice__code=self.invoice_data['code'])
+        self.assertEqual(new_item.details, new_detail)
 
     def test_delete_item(self):
-        item = self.data
+        item = Item.objects.get(invoice__code=self.invoice_data['code'])
         item.delete()
 
         data = Item.objects.all()
         self.assertEqual(data.count(), 0)
+
+
+class InvoiceTestCaseFail(TestCase, InvoiceDataFails):
+    def setUp(self):
+        self.create_invoice()
+
+    def test_add_fail(self):
+        data = Invoice.objects.all()
+        import pdb;pdb.set_trace()
