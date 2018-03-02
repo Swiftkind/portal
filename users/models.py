0105ffc1e1f076cef3933ee2c354ab6565282587
mich71095 <michaelpsy71095@gmail.com>
2018-03-02 17:32:36 +0800

    Fixes:  Extend the default User model

--- users/models.py
+++ users/models.py
@@ -1,44 +1,38 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 
 
 class UserManager(BaseUserManager):
     """Manage user
     """
-    def _create_user(self, email, first_name, last_name, password, **kwargs):
-        # Create usera
-        if not email:
-            raise ValueError('The given Email must be set')
-        email = self.normalize_email(email)
-        first_name = first_name
-        last_name = last_name
+    def create_user(self, email, first_name, last_name, password):
         user = self.model(
-            email=email, 
-            first_name=first_name, 
-            last_name=last_name, 
-            **kwargs,
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
         )
         user.set_password(password)
-        user.save()
+        user.save(using=self._db)
 
         return user
 
-    def create_user(self, email, first_name, last_name, password, **kwargs):
-        # Set default for user
-        kwargs.setdefault('is_staff', False)
-        kwargs.setdefault('is_superuser', False)
-        kwargs.setdefault('is_active', False)
+    def create_superuser(self, email, first_name, last_name, password):
+        """ Creates superuser
+        """
+        user = self.model(
+            email = email,
+            first_name = first_name,
+            last_name = last_name,
+            is_active = True,
+            is_staff = True,
+            is_superuser = True,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
 
-        return self._create_user(email, first_name, last_name, password, **kwargs)
-
-    def create_superuser(self, email, first_name, last_name, password, **kwargs):
-        # Set default for superuser
-        kwargs.setdefault('is_staff', True)
-        kwargs.setdefault('is_superuser', True)
-        kwargs.setdefault('is_active', True)
-
-        return self._create_user(email, first_name, last_name, password, **kwargs)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -48,7 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
 
-    is_active = models.BooleanField(_('activate'), default=False)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
@@ -58,11 +52,11 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
 
     def __str__(self):
-        # String  representation of the user model
-
+        """ String  representation of the user model
+        """
         return f"{self.email}"
 
     def get_full_name(self):
-        # User full name
-
+        """ User full name
+        """
         return f"{self.first_name} {self.last_name}"
