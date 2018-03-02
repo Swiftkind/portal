dc82973d7d7a3bbd409a6c6b48e230121447608f
mich71095 <michaelpsy71095@gmail.com>
2018-03-02 11:20:53 +0800

    Add users in INSTALLED_APPS and set User in AUTH_USER_MODEL

--- portal/settings.py
+++ portal/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Apps
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
