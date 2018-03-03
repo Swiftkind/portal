1a599008cd6fa14fb87152342089cdacda2765e0
cholorecto <pocholorecto123@gmail.com>
2018-03-03 13:22:01 +0800

    Refactor models_test in invoice app

--- invoices/tests/models_test.py
+++ invoices/tests/models_test.py
@@ -1,10 +1,11 @@
+import datetime
+from django.db import IntegrityError
 from django.test import TestCase
 from invoices.models import Invoice, Item
-import datetime
 
 
 class InvoiceData(object):
-    """ Invoice data and create invoice
+    """ Data for invoice model
     """
     date = datetime.datetime.now()
     invoice_data = {
@@ -36,36 +37,22 @@ class InvoiceData(object):
         return data
 
 
-class InvoiceDataFails(object):
-    """ Invoice data with errors and create invoice
+class ItemData(InvoiceData):
+    """ Data for item model
     """
-    date = datetime.datetime.now()
-    invoice_data = {
-        'code':None,
-        'order_id':"12",
-        'invoice_date':None,
-        'terms':"Due of Receipt",
-        'due_date':None,
-        'notes':"sample notes",
-        'conditions':"condition 1",
-        'date_created':date,
-        'date_updated':date,
-        'status':"Sent"
+    item_data = {
+        'details':"Detail sample",
+        'quantity':3,
+        'rate':45
     }
-
-    def create_invoice(self):
-        data = Invoice.objects.create(
-                code = self.invoice_data['code'],
-                order_id = self.invoice_data['order_id'],
-                invoice_date = self.invoice_data['invoice_date'],
-                terms = self.invoice_data['terms'],
-                due_date = self.invoice_data['due_date'],
-                notes = self.invoice_data['notes'],
-                conditions = self.invoice_data['conditions'],
-                date_created = self.invoice_data['date_created'],
-                date_updated = self.invoice_data['date_updated'],
-                status = self.invoice_data['status']
-            )
+    def create_item(self):
+        invoice = self.create_invoice()
+        data = Item.objects.create(
+            invoice = invoice,
+            details = self.item_data['details'],
+            quantity = self.item_data['quantity'],
+            rate = self.item_data['rate']
+        )
         return data
 
 
@@ -74,14 +61,14 @@ class InvoiceTestCaseSuccess(TestCase, InvoiceData):
     """
     def setUp(self):
         self.create_invoice()
+        self.invoice = Invoice.objects.get(code=self.invoice_data['code'])
 
     def test_add_invoice(self):
-        invoice = Invoice.objects.get(code=self.invoice_data['code'])
-        self.assertEqual(invoice.code, self.invoice_data['code'])
+        self.assertEqual(self.invoice.code, self.invoice_data['code'])
 
     def test_edit_invoice(self):
         new_code = "new_code"
-        invoice = Invoice.objects.get(code=self.invoice_data['code'])
+        invoice = self.invoice
         invoice.code = new_code
         invoice.order_id = "11"
         invoice.save()
@@ -89,34 +76,22 @@ class InvoiceTestCaseSuccess(TestCase, InvoiceData):
         self.assertEqual(new_invoice.code, new_code)
 
     def test_delete_invoice(self):
-        invoice = Invoice.objects.get(code=self.invoice_data['code'])
+        invoice = self.invoice
         invoice.delete()
 
         data = Invoice.objects.all()
         self.assertEqual(data.count(), 0)
 
 
-class InvoiceItemTestCaseSuccess(TestCase, InvoiceData):
+class InvoiceItemTestCaseSuccess(TestCase, ItemData):
     """ Test case for Item in voice
     """
     def setUp(self):
-        invoice = self.create_invoice()
-        item_data = {
-            'invoice':invoice,
-            'details':"Detail sample",
-            'quantity':3,
-            'rate':45
-        }
-        Item.objects.create(
-            invoice = item_data['invoice'],
-            details = item_data['details'],
-            quantity = item_data['quantity'],
-            rate = item_data['rate']
-        )
+        self.create_item()
+        self.item = Item.objects.get(invoice__code=self.invoice_data['code'])
 
     def test_add_item(self):
-        item = Item.objects.get(invoice__code=self.invoice_data['code'])
-        self.assertEqual(item.invoice.code, self.invoice_data['code'])
+        self.assertEqual(self.item.invoice.code, self.invoice_data['code'])
 
     def test_edit_item(self):
         new_detail = "New detail"
@@ -128,17 +103,87 @@ class InvoiceItemTestCaseSuccess(TestCase, InvoiceData):
         self.assertEqual(new_item.details, new_detail)
 
     def test_delete_item(self):
-        item = Item.objects.get(invoice__code=self.invoice_data['code'])
+        item = self.item
         item.delete()
 
         data = Item.objects.all()
         self.assertEqual(data.count(), 0)
 
 
-class InvoiceTestCaseFail(TestCase, InvoiceDataFails):
+class InvoiceTestCaseFail(TestCase, InvoiceData):
+    """ Test invoice fails
+    """
     def setUp(self):
-        self.create_invoice()
+        self.invoice = self.create_invoice()
 
     def test_add_fail(self):
-        data = Invoice.objects.all()
-        import pdb;pdb.set_trace()
+
+        def create_invoice_fail():
+            try:
+                Invoice.objects.create(
+                    code = None,
+                    order_id = self.invoice_data['order_id'],
+                    invoice_date = self.invoice_data['invoice_date'],
+                    terms = None,
+                    due_date = self.invoice_data['due_date'],
+                    notes = self.invoice_data['notes'],
+                    conditions = self.invoice_data['conditions'],
+                    date_created = self.invoice_data['date_created'],
+                    date_updated = self.invoice_data['date_updated'],
+                    status = self.invoice_data['status']
+                )
+            except IntegrityError:
+                return False
+            return True
+
+        data = create_invoice_fail()
+        self.assertEqual(data, False)
+
+    def test_edit_fail(self):
+
+        def edit_invoice_fail():
+            try:
+                self.invoice.code = None
+                self.invoice.save()
+            except IntegrityError:
+                return False
+            return True
+
+        data = edit_invoice_fail()
+        self.assertEqual(data, False)
+
+
+class InvoiceItemTestCaseFail(TestCase, ItemData):
+    """ Item test fails
+    """
+    def setUp(self):
+        self.item = self.create_item()
+
+    def test_add_fail(self):
+        def create_item_fail():
+            try:
+                Item.objects.create(
+                invoice = None,
+                details = self.item_data['details'],
+                quantity = self.item_data['quantity'],
+                rate = self.item_data['rate']
+            )
+            except IntegrityError:
+                return False
+            return True
+
+        data = create_item_fail()
+        self.assertEqual(data, False)
+
+    def test_edit_fail(self):
+        def edit_item_fail():
+            try:
+                self.item.invoice = None
+                self.item.details = None
+                self.item.save() 
+            except IntegrityError:
+                return False
+            return True
+
+        data = edit_item_fail()
+        self.assertEqual(data, False)
