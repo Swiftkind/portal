5f893f97610195984ad714680a1eb4e24f2c6a31
cholorecto <pocholorecto123@gmail.com>
2018-03-06 14:58:05 +0800

    Added user login and fix user admin

---
+++ users/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth import authenticate
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    error_msg = "Invalid Email/Password."
+
+    def clean(self):
+        """ validate user's credentials
+        """
+        email = self.cleaned_data.get('email')
+        password = self.cleaned_data.get('password')
+        # check if user's credentials are valid
+        self.user = authenticate(email=email, password=password)
+        if self.user is None or not self.user.is_active:
+            raise forms.ValidationError(self.error_msg, code='400')
+        return self.cleaned_data
