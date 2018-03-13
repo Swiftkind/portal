f300de8ca2561c72a8862b38a3d6e56aec84024f
cholorecto <pocholorecto123@gmail.com>
2018-03-13 16:27:11 +0800

    Fixed the static data in invoice template

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -1,18 +1,25 @@
 from rest_framework import serializers
 from invoices.models import Invoice, Item
+from customers.serializers import CustomerSerializer
+from customers.models import Customer
 
 
 class InvoiceSerializer(serializers.ModelSerializer):
 
     items = serializers.SerializerMethodField()
+    customer = serializers.SerializerMethodField()
 
     class Meta:
         model = Invoice
         fields = ('id','code','order_id','invoice_date','terms','due_date',
-                  'notes','conditions','status','items','total_amount')
+                  'notes','conditions','status','items','total_amount','customer')
 
     def get_items(self, obj):
         serializer = ItemSerializer(Item.objects.filter(invoice=obj), many=True)
+        return serializer.data
+
+    def get_customer(self, obj):
+        serializer = CustomerSerializer(Customer.objects.get(email=obj.customer.email))
         return serializer.data
 
 
