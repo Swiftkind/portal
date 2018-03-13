30271dfaf1a45e84bfbd42f33553c31b993996df
jkingz <jfkrule@gmail.com>
2018-03-13 14:12:19 +0800

    Add functionality for sidebar user details

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,4 +6,13 @@ $(document).ready(function () {
 	$('.datepicker').datepicker({
         uiLibrary: 'bootstrap4'
     });
+
+    $('#profile').on('click', function(e) {
+        e.preventDefault();
+        $('#profile-side-bar').toggleClass('active');
+    });
+
+    $('.side-bar-toggle').on('click', function() {
+        $('#profile-side-bar').removeClass('active');
+    });
 });
