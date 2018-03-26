f7466fb545c54385da41e7c812f0909c7c7983f5
jkingz <jfkrule@gmail.com>
2018-03-26 17:58:47 +0800

    Set api url inside the module

--- assets/frontend/users/init.js
+++ assets/frontend/users/init.js
@@ -4,6 +4,7 @@
     angular
         .module('users.portal', [])
         .constant('TEMPLATE_URL', '/static/frontend/templates/users/')
+        .constant('API_URL', '/api/users/auth/')
     ;
 
 })();
