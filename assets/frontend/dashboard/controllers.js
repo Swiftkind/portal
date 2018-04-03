341114c5703ed69eb38daaa5c910244a7f2bd811
cholorecto <pocholorecto123@gmail.com>
2018-04-03 16:03:41 +0800

    Sorts the data

--- assets/frontend/dashboard/controllers.js
+++ assets/frontend/dashboard/controllers.js
@@ -9,11 +9,26 @@
      */
     function DashboardController($scope, InvoiceService){
       self = this;
+      self.order = 0;
       self.invoiceService = InvoiceService;
       feather.replace();
 
+      // Request to backend
       self.sortBy = function (field, order) {
         self.invoiceService.getList('', field, order);
+      }
+
+      // Sorts the display 
+      self.sort = function (field) {
+        self.order = self.order == 0 ? 1 : 0;
+
+        if (self.order === 0) {
+          self.sortBy(field, 'asc');
+
+        } else if (self.order === 1) {
+          self.sortBy(field, 'desc');
+          
+        }
       }
     }
 
