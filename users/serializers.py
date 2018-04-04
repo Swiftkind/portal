212981d6afc655872fc0f609cacaf51a2c530635
jkingz <jfkrule@gmail.com>
2018-04-05 01:00:53 +0800

    Create user signup endpoint

--- users/serializers.py
+++ users/serializers.py
@@ -59,3 +59,28 @@ class LoginSerializer(serializers.Serializer):
         token, create = Token.objects.get_or_create(user=user)
         user_data['token'] = token.key
         return user_data
+
+
+class SignUpSerializer(serializers.Serializer):
+    """ User SignUp Serializer
+    """
+    email = serializers.EmailField()
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+    password = serializers.CharField()
+    confirm_password = serializers.CharField()
+
+    def validate_email(self, email):
+        existing = User.objects.filter(email=email).first()
+        if existing:
+            raise serializers.ValidationError("Someone have already own that email")
+        return email
+
+    def validate(self, data):
+        if not data.get('password') or not data.get('confirm_password'):
+            raise serializers.ValidationError("Please enter a password and " "confirm it")
+
+        if data.get('password') != data.get('confirm_password'):
+            raise serializers.ValidationError("Password don't match!")
+        return data
+
