8f4f115e7960210c302dfbc5d607a5f3a11568e6
jkingz <jfkrule@gmail.com>
2018-04-09 22:53:27 +0800

    Fix dashboard sorting functionality

--- assets/frontend/dashboard/controllers.js
+++ assets/frontend/dashboard/controllers.js
@@ -12,22 +12,32 @@
    function DashBoardController($scope, InvoiceService) {
     feather.replace();
     var self = this;
-    var order = 0;
+    var order = 'asc';
 
     self.invoiceService = InvoiceService;
 
-    // request to the backend
+    self.sorting = {
+      'customer': false,
+      'code': false,
+      'due_date': false,
+      'status': false,
+    };
+
+    //request to the backend
     self.sortBy = function(field, order) {
-      self.invoiceService.getList('', field, order);
+      var page_number = {"page": self.invoiceService.list.page.number}
+      self.invoiceService.getList(page_number, field, order);
     };
 
     // Sort the display
     self.sort = function (field) {
-      self.order = self.order == 0 ? 1 : 0;
+      feather.replace();
+      self.sorting[field] = !self.sorting[field];
+      self.order = self.order == 'asc' ? 'desc' : 'asc';
 
-      if(self.order === 0) {
+      if(self.order === 'asc') {
         self.sortBy(field, 'asc');
-      } else if (self.order === 1) {
+      } else if (self.order === 'desc') {
         self.sortBy(field, 'desc');
       };
     };
